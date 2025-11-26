b34065913708d4f8706e3416895e6bbf211b44d7
goldyy12 <diarselmani10@gmail.com>
2025-11-26 16:21:02 +0100

    Changes to POOL

--- db/pool.js
+++ db/pool.js
@@ -8,14 +8,7 @@ if (process.env.DATABASE_URL) {
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }, // required in cloud
   });
-} else {
-  pool = new Pool({
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
-  });
 }
+
 
 module.exports = pool;
