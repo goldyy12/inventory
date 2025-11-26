f1752c9b221572ee1a93a10ea8e88629860348ae
goldyy12 <diarselmani10@gmail.com>
2025-11-26 16:14:31 +0100

    Changes to POOL

--- db/pool.js
+++ db/pool.js
@@ -4,22 +4,18 @@ require("dotenv").config();
 let pool;
 
 if (process.env.DATABASE_URL) {
-    // Cloud deployment (Render, Railway, etc.)
-    pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false, // needed for cloud Postgres
-        },
-    });
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, // required in cloud
+  });
 } else {
-    // Local development
-    pool = new Pool({
-        host: process.env.PGHOST,
-        user: process.env.PGUSER,
-        database: process.env.PGDATABASE,
-        password: process.env.PGPASSWORD,
-        port: process.env.PGPORT,
-    });
+  pool = new Pool({
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
+  });
 }
 
 module.exports = pool;
