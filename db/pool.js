273b3238b50da7ccc75ffaae602d71109c31bd33
goldyy12 <diarselmani10@gmail.com>
2025-12-04 01:24:45 +0100

    db

--- db/pool.js
+++ db/pool.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: false,
 });
 
 module.exports = pool;
