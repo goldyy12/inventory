7e51fecf83996c67d33401496ef4f90f7679d17d
goldyy12 <diarselmani10@gmail.com>
2025-12-04 01:51:47 +0100

    db

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
-const usersRouter = require("./routers/router")
+const usersRouter = require("./routers/router");
 const path = require("path");
 
 
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 
-app.use("/", usersRouter)
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", usersRouter);
+
 app.get("/", (req, res) => {
     res.redirect("/categories");
 });
@@ -20,4 +23,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
-app.use(express.static(path.join(__dirname, "public")));
--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,17 @@
-
-// db/connection.js
 const { Pool } = require('pg');
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined. Check your Render environment settings.");
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
