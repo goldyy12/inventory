d75e5fdb177fbf56361b6f34981eb60a9d86492a
goldyy12 <diarselmani10@gmail.com>
2025-12-04 01:37:21 +0100

    db

--- db/pool.js
+++ db/pool.js
@@ -1,20 +1,11 @@
+
+// db/connection.js
 const { Pool } = require('pg');
 
-// Only load .env locally, not in production
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
-let connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  // fallback for local dev
-  connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
-}
 
 const pool = new Pool({
-  connectionString,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
